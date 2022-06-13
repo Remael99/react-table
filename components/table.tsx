0ad770dd00b912f5058eb8828a03fc7c7fdb198f
Remael99 <remael99@gmail.com>
2022-06-13 16:21:30 +0300

    update: react table

---
+++ components/table.tsx
@@ -0,0 +1,21 @@
+//@ts-nocheck
+import React from 'react'
+import {
+    createTable,
+    getCoreRowModel,
+    useTableInstance,
+  } from 'react-table'
+import { makeData, Person } from '../utils/make_data';
+
+
+export default function Table() {
+
+  
+    return (
+  
+        <table className='bg-gray-50 border border-gray-100 w-[90%] rounded-sm shadow sm   h-[600px]'>
+        hello
+        </table>
+
+    )
+}
